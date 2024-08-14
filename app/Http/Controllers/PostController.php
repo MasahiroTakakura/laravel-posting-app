0219a552fe1d2d94688b773de0685c95b20975ef
MasahiroTakakura <m.toh0403mame@icloud.com>
2024-08-15 00:38:34 +0900

    投稿一覧ページの表示順を更新日時が古い順になるように変更

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,11 +11,11 @@ class PostController extends Controller
 {
     // 一覧ページ
     public function index()
-     {
-         $posts = Auth::user()->posts()->orderBy('created_at', 'desc')->get();
- 
-         return view('posts.index', compact('posts'));
-     }
+    {
+        $posts = Auth::user()->posts()->orderBy('updated_at', 'asc')->get(); // 更新日時が古い順に並べ替え
+    
+        return view('posts.index', compact('posts'));
+    }
 
     //  詳細ページ
     public function show(Post $post)
